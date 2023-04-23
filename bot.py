f1f751f27492c1a52526d1d50010e1c9fb6f9f7a
Lison Lau <allisonlhm@gmail.com>
2023-04-23 13:38:28 +0800

    not done

--- bot.py
+++ bot.py
@@ -1,6 +1,20 @@
 import ccxt
 import ta
 import pandas as pd
+import random
+
+# Define the range of parameter values for the MACD indicator
+PARAMETER_RANGES = {
+    'window_slow': range(10, 41),
+    'window_fast': range(1, 21),
+    'window_sign': range(1, 21)
+}
+
+# Define genetic algorithm parameters
+POPULATION_SIZE = 20
+NUM_GENERATIONS = 10
+MUTATION_RATE   = 0.1
+CROSSOVER_RATE  = 0.8
 
 # Retrieve OHLCV data
 def getOHLCVdata():
@@ -14,57 +28,135 @@ def getOHLCVdata():
     data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
     return data
 
-# Calculate MACD indicator using the ta library
-def calculateMACD(data):
-    data['macd'] = ta.trend.MACD(data['close']).macd()
-    return data
-
+# Define the buy trigger function using MACD indicator
 def buy_trigger(t, P, data):
-    # get MACD values for previous and current days
-    macd_prev = ta.trend.MACD(data['close'][t-1], window_slow=P[0], window_fast=P[1], window_sign=P[2]).macd_signal()[t-2]
-    macd_curr = ta.trend.MACD(data['close'][t], window_slow=P[0], window_fast=P[1], window_sign=P[2]).macd_signal()[t-1]
-    # get signal values for previous and current days
-    signal_prev = ta.trend.MACD(data['close'][t-1], window_slow=P[0], window_fast=P[1], window_sign=P[2]).macd()[t-2]
-    signal_curr = ta.trend.MACD(data['close'][t], window_slow=P[0], window_fast=P[1], window_sign=P[2]).macd()[t-1]
-    # generate buy signal if MACD crosses above signal and both are positive
-    return (macd_prev < signal_prev and macd_curr > signal_curr and macd_curr > 0 and signal_curr > 0)
-
-def sell_trigger(t, P, data):
-    # get MACD values for previous and current days
-    macd_prev = ta.trend.MACD(data['close'][t-1], window_slow=P[0], window_fast=P[1], window_sign=P[2]).macd_signal()[t-2]
-    macd_curr = ta.trend.MACD(data['close'][t], window_slow=P[0], window_fast=P[1], window_sign=P[2]).macd_signal()[t-1]
-    # get signal values for previous and current days
-    signal_prev = ta.trend.MACD(data['close'][t-1], window_slow=P[0], window_fast=P[1], window_sign=P[2]).macd()[t-2]
-    signal_curr = ta.trend.MACD(data['close'][t], window_slow=P[0], window_fast=P[1], window_sign=P[2]).macd()[t-1]
-    # generate sell signal if MACD crosses below signal and both are negative
-    return (macd_prev > signal_prev and macd_curr < signal_curr and macd_curr < 0 and signal_curr < 0)
-
-# Define the bot function
-# P format = (window_slow, window_fast, window_sign)
-def bot(P):
-    data = getOHLCVdata()
-    data = calculateMACD(data)
+    # Define MACD parameters from P
+    w_slow = int(P[0])
+    w_fast = int(P[1])
+    w_sign = int(P[2])
     
-    # Initialise holdings
+    # Get close prices data for MACD calculation
+    prices = data.loc[:t, 'close']
+    
+    # Calculate MACD and signal lines for t
+    macd_indicator = ta.trend.MACD(close=prices, window_slow=w_slow, window_fast=w_fast, window_sign=w_sign)
+    macd_line      = macd_indicator.macd().loc[t]
+    signal_line    = macd_indicator.macd_signal().loc[t]
+    prev_macd      = macd_indicator.macd().loc[t-1] 
+    prev_signal    = macd_indicator.macd_signal().loc[t-1]
+    # prev_signal <= prev_macd
+    # Trigger buy signal if MACD line is above signal line and both are positive
+    return (macd_line > signal_line)
+
+# Define the sell trigger function using MACD indicator
+def sell_trigger(t, P, data):
+    # Define MACD parameters from P
+    w_slow = int(P[0])
+    w_fast = int(P[1])
+    w_sign = int(P[2])
+    
+    # Get close prices data for MACD calculation
+    prices = data.loc[:t, 'close']
+    
+    # Calculate MACD and signal lines for t
+    macd_indicator = ta.trend.MACD(close=prices, window_slow=w_slow, window_fast=w_fast, window_sign=w_sign)
+    macd_line      = macd_indicator.macd().loc[t]
+    signal_line    = macd_indicator.macd_signal().loc[t]
+    prev_macd      = macd_indicator.macd().loc[t-1] 
+    prev_signal    = macd_indicator.macd_signal().loc[t-1]
+    # prev_signal >= prev_macd
+    # Trigger sell signal if MACD line is above signal line
+    return (macd_line < signal_line)
+
+# Define the trading bot function
+# P format = (window_slow, window_fast, window_sign)
+def trading_bot(P, data):
+    # Initialise variables
     AUD = 100.0     # starting AUD holdings
     BTC = 0.0       # starting BTC holdings
     buy_triggered = False
 
+    # Loop through each day in the data
     for t in range(len(data)):
+        # Check if a buy trigger occurs and there is no concurrent sell trigger
         if buy_trigger(t, P, data) and not buy_triggered:
             buy_triggered = True
-            BTC = AUD * (0.98)
+            BTC = AUD * 0.98 / data['close'][t]
             AUD = 0.0
+        # Check if a sell trigger occurs and there is no concurrent buy trigger
         elif sell_trigger(t, P, data) and buy_triggered:
             buy_triggered = False
-            AUD = BTC  * (0.98)
+            AUD = BTC  * 0.98 * data['close'][t]
             BTC = 0.0
+        # Sell remaining BTC at the end of the test period
         elif t == len(data)-1 and BTC > 0:
-            AUD = BTC * (0.98)
+            AUD = BTC * 0.98 * data['close'][t]
             BTC = 0.0
     return AUD
 
+# Define the fitness function
+def evaluate_fitness(bot_final_AUD):
+    profit = bot_final_AUD - 100.0
+    return profit
 
+# Define the crossover function
+def crossover(parent1, parent2):
+    child1 = parent1.copy()
+    child2 = parent2.copy()
+    if random.random() < CROSSOVER_RATE:
+        crossover_point = random.randint(1, len(parent1[1]) - 1)
+        child1[1][:crossover_point] = parent2[1][:crossover_point]
+        child2[1][:crossover_point] = parent1[1][:crossover_point]
+    return [child1, child2]
 
+# Define the mutation function
+def mutation(bot):
+    mutated_bot = bot.copy()
+    for i in range(len(mutated_bot[1])):
+        if random.random() < MUTATION_RATE:
+            mutated_bot[1][i] = random.randint(P_MIN, P_MAX)
+    return mutated_bot
+
+# Genetic algorithm
+def genetic_algorithm():
+    # Get same set of data used for all bots 
+    data = getOHLCVdata()
+    
+    # Generate an initial population of bots with different parameters
+    population = []
+    for n in range(POPULATION_SIZE):
+        P = ()
+        bot = trading_bot(P, data)
+        population.append(bot) 
+        
+    # Run the genetic algorithm
+    for g in range(NUM_GENERATIONS):
+        # Evaluate the fitness of each bot in the population
+        fitness_scores = [evaluate_fitness(bot) for bot in population]
+        
+        # Select the top performing bots
+        elite_indices = sorted(range(len(fitness_scores)), key=lambda i: fitness_scores[i], reverse=True)[:POPULATION_SIZE // 2]
+        elite_bots = [population[i] for i in elite_indices]
+        
+        # Apply genetic operators to create a new population
+        new_population = elite_bots.copy()
+        while len(new_population) < POPULATION_SIZE:
+            parent1 = random.choice(elite_bots)
+            parent2 = random.choice(elite_bots)
+            child1, child2 = crossover(parent1, parent2)
+            new_population.extend([mutation(child1), mutation(child2)])
+            
+        # Replace the old population with the new one
+        population = new_population
+        
+    # Evaluate the fitness of the final population
+    fitness_scores = [evaluate_fitness(bot) for bot in population]
+    
+    # Select the best performing bot and print its parameters and final value
+    best_bot_index = fitness_scores.index(max(fitness_scores))
+    best_bot = population[best_bot_index]
+    return best_bot
+    
+    
 
 
