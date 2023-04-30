80ffb7d2d96f5d93460c9fff0f741b076356c0ff
Lison Lau <allisonlhm@gmail.com>
2023-04-30 18:27:21 +0800

    implement bb and sma indicator

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import random
 
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
+
 class TradingBot:
     
     # Constructor for TradingBot
@@ -26,21 +27,22 @@ class TradingBot:
         for i in range(len(P)):
             lit = 1
             if (P[i][0] == "macd"):
-                a = self.macd_indicator(t,P[i])
-                lit = a[0] > a[1] and a[2] < a[3]
+                a = self.macd_indicator(t)
+                lit = a[0] > a[1] and a[2] < a[3] and a[0] > 0
             elif (P[i][0] == "bb"):
-                pass
+                a = self.bb_indicator(t)
+                lit = a[0] > self.data['close'][t] and a[2] < self.data['close'][t-1]
             elif (P[i][0] == "rsi"):
-                a = self.rsi_indicator(t, P[i])
+                a = self.rsi_indicator(t)
                 lit = a.item() < RSI_OVERSOLD
-            elif (P[i][0] == "obv"):
-                pass
-            
-            if (i == 0): # for the first literal
+            elif (P[i][0] == "sma"):
+                a = self.sma_indicator(t)
+                lit = a[0] > self.data['close'][t] and a[1] <= self.data['close'][t-1]
+            if (i == 0):            # for the first literal
                 dnf = lit
-            elif(P[i][1]==1): # it is an OR conditional
+            elif(P[i][1]==1):       # it is an OR conditional
                 dnf = dnf or lit 
-            else: # it is an AND conditional
+            else:                   # it is an AND conditional
                 dnf = dnf and lit
         return dnf
     
@@ -49,27 +51,27 @@ class TradingBot:
         dnf = 1
         for i in range(len(P)):
             if (P[i][0] == "macd"):
-                a = self.macd_indicator(t,P[i])
-                lit = a[0] < a[1] and a[2] > a[3]
-            elif (P[i][0] == "bb"):
-                a = self.bb_indicator(t, P[i])
-                pass
+                a = self.macd_indicator(t)
+                lit = a[0] < a[1] and a[2] > a[3] and a[0] < 0
             elif (P[i][0] == "rsi"):
-                a = self.rsi_indicator(t, P[i])
-                lit = a.item() > RSI_OVERBOUGHT
-            elif (P[i][0] == "obv"):
-                pass
-
-            if (i == 0): # for the first literal
+                a = self.rsi_indicator(t)
+                lit = a.item() > RSI_OVERBOUGHT   
+            elif (P[i][0] == "sma"):
+                a = self.sma_indicator(t)
+                lit = a[0] < self.data['close'][t] and a[1] >= self.data['close'][t-1] 
+            elif (P[i][0] == "bb"):
+                a = self.bb_indicator(t)
+                lit = a[1] < self.data['close'][t] and a[3] > self.data['close'][t-1]
+            if (i == 0):            # for the first literal
                 dnf = lit
-            elif(P[i][1]): # it is an OR conditional
+            elif(P[i][1]):          # it is an OR conditional
                 dnf = dnf or lit 
-            else:
+            else:                   # it is an AND conditional
                 dnf = dnf and lit
         return dnf
 
     # MACD indicator
-    def macd_indicator(self, t, P):
+    def macd_indicator(self, t):
         macd_line      = self.data["macd"].loc[t]
         signal_line    = self.data["macd_sig"].loc[t]
         
@@ -82,29 +84,37 @@ class TradingBot:
         
         return [macd_line, signal_line, prev_macd, prev_signal]
 
-    # BB indicator    
-    def bb_indicator(self,t,P):
-        # Define Bollinger Bands parameters
-        window = int(P[2])
-        window_dev = int(P[3])
-
-        prices = self.data.loc[:t, 'close']
-        bb_ind = ta.volatility.BollingerBands(close=prices, window=window, window_dev=window_dev)
-        
     # RSI indicator
-    def rsi_indicator(self,t,P):
-        # Get close prices for RSI
+    def rsi_indicator(self, t):
         if t-1 > 0:
             rsi_line  = self.data["rsi"].loc[t]
         else:
             rsi_line  = self.data["rsi"].loc[0]
         return rsi_line 
 
-    def sma_indicator(self,t,P):
-        pass
+    # BB indicator    
+    def bb_indicator(self, t):
+        lower_band = self.data["bb_lower"][t]
+        upper_band = self.data["bb_upper"][t]
+        
+        if t-1 > 0:
+            prev_lower = self.data["bb_lower"][t-1]
+            prev_upper = self.data["bb_upper"][t-1]
+        else:
+            prev_lower = self.data["bb_lower"][0]
+            prev_upper = self.data["bb_upper"][0]
+        return [lower_band, upper_band, prev_lower, prev_upper]
 
-    # if need make for o,h,l,c,v data
-    def candle_value(self,t):
+    def sma_indicator(self, t):
+        current = self.data["sma"].loc[t]
+        if t-1 > 0:
+            previous = self.data["sma"].loc[t-1]
+        else:
+            previous = self.data["sma"].loc[0]
+        return [current, previous]
+
+    # If need make for OHLVC data
+    def candle_value(self, t):
         pass
 
     # Run the trading bot
@@ -114,6 +124,8 @@ class TradingBot:
         BTC = 0.0       # starting BTC holdings
         buy_triggered = False
         prices = self.data['close']
+        
+        # Store indicators in the dataframe
         for p in self.P:
             type = p[0]
             if type =="macd":
@@ -121,9 +133,16 @@ class TradingBot:
                 self.data["macd"] = macd_indicator.macd()
                 self.data["macd_sig"] = macd_indicator.macd_signal()
             elif type == "rsi":
-                rsi_indicator = ta.momentum.RSIIndicator(close = prices, window = p[2])
+                rsi_indicator = ta.momentum.RSIIndicator(close=prices, window=p[2])
                 self.data["rsi"] = rsi_indicator.rsi()
-
+            elif type == "bb":
+                bb_indicator = ta.volatility.BollingerBands(close=prices, window=p[2], window_dev=p[3])
+                self.data["bb_lower"] = bb_indicator.bollinger_lband()
+                self.data["bb_upper"] = bb_indicator.bollinger_hband()
+            elif type == "sma":
+                sma_indicator = ta.trend.SMAIndicator(close=prices, window=p[2])
+                self.data["sma"] = sma_indicator.sma_indicator()
+        
         # Loop through each day in the data
         for t in range(len(self.data)):
             # Check if a buy trigger occurs and there is no concurrent sell trigger
@@ -145,28 +164,23 @@ class TradingBot:
         return AUD
     
     
+def getOHLCVdata():
+    # Initialize the Kraken exchange
+    kraken = ccxt.kraken()
+    # Retrieve the historical data for BTC/AUD from the Kraken exchange
+    ohlcv = kraken.fetch_ohlcv('BTC/AUD', '1d')
+    # Convert the data to a pandas DataFrame
+    data = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    # Convert the timestamp to a datetime object
+    data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
+    return data
 
-# def getOHLCVdata():
-#     # Initialize the Kraken exchange
-#     kraken = ccxt.kraken()
-#     # Retrieve the historical data for BTC/AUD from the Kraken exchange
-#     ohlcv = kraken.fetch_ohlcv('BTC/AUD', '1d')
-#     # Convert the data to a pandas DataFrame
-#     data = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-#     # Convert the timestamp to a datetime object
-#     data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
-#     return data
-
-# data = getOHLCVdata()
-# # data['rsi'] = ta.momentum.RSIIndicator(data['close']).rsi()
-# # data['macd'] = ta.trend.MACD(data['close']).macd()
-# # data['macd_signal'] = ta.trend.MACD(data['close']).macd_signal()
-# # print(data[10:70])
-# # a = TradingBot([["macd", 1, 26, 12, 9]],data)
-# a = TradingBot([["macd", 1, 26, 12, 9]], data)
-# aud = a.run()
-# print(aud)
+data = getOHLCVdata()
+a = TradingBot([["bb", 1, 20, 2], ["macd", 1, 26, 12, 9], ["rsi", 1, 14], ["bb", 1, 20, 2]], data)
+aud = a.run()
+print(aud)
 
 # MACD  = ["macd", 1, 26, 12, 9]
-# RSI   = ["rsi", 1 14]
-# BB    = ["bb", 1, ]
+# RSI   = ["rsi", 1, 14]
+# BB    = ["bb", 1, 20, 2]
+# SMA   = ["sma", 1, 20]
